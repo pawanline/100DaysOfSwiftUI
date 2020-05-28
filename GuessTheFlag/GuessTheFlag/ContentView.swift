a380446a2bd723ef5975899d9cadadb25d8458e1
Pawan Kumar <pawan.kumar@Apples-MacBook-Pro.local>
2020-05-29 01:12:54 +0530

    Day 21-part1

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,15 +10,35 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showingAlert = false
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+
+
     var body: some View {
-        Button("showing Alert") {
-            self.showingAlert = true
-        }
-        .alert(isPresented: $showingAlert) {
-            Alert(title: Text("This is a test Alert"), message: Text("Are you liking the alert?"), dismissButton: .default(Text("Ok")))
-        }
+        ZStack {
+            Color.blue.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                     .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                     .foregroundColor(.white)
+                }
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                        // flag was tapped
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                    }
+
+                }
+                Spacer()
+
+            }
         }
     }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
