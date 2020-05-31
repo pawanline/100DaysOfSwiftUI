64d1ed236f05295a8fcc89de8570977679d37e0c
Pawan Kumar <pawan.kumar@Apples-MacBook-Pro.local>
2020-06-01 02:51:39 +0530

    Custom Containers

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -8,63 +8,45 @@
 
 import SwiftUI
 
-struct  Title: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundColor(.white)
-            .padding()
-            .background(Color.blue)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-    }
-}
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
 
-
-extension View {
-    func titleStyle() -> some View {
-        self.modifier(Title())
-    }
-}
-struct CapsuleText: View {
-    var text: String
-    
-    var body: some View {
-        Text(text)
-        .font(.largeTitle)
-        .padding()
-            
-        .foregroundColor(.white)
-        .background(Color.blue)
-            .clipShape(Capsule())
-    }
-}
-
-struct Watermark: ViewModifier {
-    var text: String
-
-    func body(content: Content) -> some View {
-        ZStack(alignment: .bottomTrailing) {
-            content
-            Text(text)
-                .font(.caption)
-                .foregroundColor(.white)
-                .padding(5)
-                .background(Color.black)
+    // Approach 2
+//    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
+//        self.rows = rows
+//        self.columns = columns
+//        self.content = content
+//    }
+   var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<self.columns, id: \.self) { column in
+                        self.content(row, column)
+                    }
+                }
+            }
         }
     }
 }
 
-extension View {
-    func watermarked(with text: String) -> some View {
-        self.modifier(Watermark(text: text))
-    }
-}
+
 struct ContentView: View {
     var body: some View {
-        Color.blue
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .watermarked(with: "Appophilte Tech")
-}
+      GridStack(rows: 4, columns: 4) { row, col in
+            HStack {
+                Image(systemName: "\(row * 4 + col).circle")
+                Text("R\(row) C\(col)")
+            }
+        }
+        
+//        GridStack(rows: 4, columns: 4) { row, col in
+//            Image(systemName: "\(row * 4 + col).circle")
+//            Text("R\(row) C\(col)")
+//        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
