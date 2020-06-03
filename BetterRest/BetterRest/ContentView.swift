c4eb49c603464666fa49b554d025b0c3d9e8e040
Appophile <appophiletech@gmail.com>
2020-06-04 01:08:50 +0530

    Stepper view

---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Pawan Kumar on 04/06/20.
+//  Copyright © 2020 Pawan Kumar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var sleepAmount = 8.0
+    var body: some View {
+        Stepper(value: $sleepAmount,in: 4...12,step: 0.25) {
+            Text(" \(sleepAmount,specifier: "%.2g") hours")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
