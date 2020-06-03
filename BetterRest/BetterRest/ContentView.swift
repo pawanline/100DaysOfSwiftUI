dbea120723a5864e399e0de3fdf968a6ed970195
Appophile <appophiletech@gmail.com>
2020-06-04 01:40:06 +0530

    Date formatter added

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -10,13 +10,31 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var sleepAmount = 8.0
+    @State var wakeUpDate = Date()
     var body: some View {
-        Stepper(value: $sleepAmount,in: 4...12,step: 0.25) {
-            Text(" \(sleepAmount,specifier: "%.2g") hours")
+//        let now = Date()
+//        let tomorrow = now.addingTimeInterval(86400)
+        DatePicker("Please enter a Date", selection: $wakeUpDate, in: Date()...)
+        .labelsHidden()
+        
         }
+    func dateformatting() {
+        var components = DateComponents()
+        components.hour = 8
+        components.minute = 0
+
+        let date = Calendar.current.date(from: components) ?? Date()
+
+        let componentsTest = Calendar.current.dateComponents([.hour,.minute], from: Date())
+        let hour = componentsTest.hour ?? 0
+        let minute = componentsTest.minute ?? 0
+
+        let fomatter = DateFormatter()
+        fomatter.dateStyle = .medium
+        let dateString = fomatter.string(from: Date())
+
     }
-}
+    }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
